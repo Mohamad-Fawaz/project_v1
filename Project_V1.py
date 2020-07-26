5a64cdf19d9cec70bad63446896cd2173762ac39
Mohamad-Fawaz <68426849+Mohamad-Fawaz@users.noreply.github.com>
2020-07-26 15:51:50 +0300

    Add files via upload

---
+++ Project_V1.py
@@ -0,0 +1,48 @@
+class electric():
+    pass
+class water():
+    pass
+def electric_fun(name):
+    elect = electric() # we creat like an object to use it with the parameters function
+    # for example :
+    # elect.name = name
+def water_fun():
+    water_ob = water()
+def choice():
+    print('1_ Show electricity production \n2_ Show water production \n3_\n ')
+    cho = input('Enter the selection number please >\n')
+    check_cho = cho.isdigit()
+    if cho == '1' :
+        print('Exeal File data for electricity production')
+    elif cho == '2' :
+        print('Exeal File data for Water production')
+    elif cho == '3' :
+        print('later')
+    elif check_cho == False :
+        print('please enter number')
+    else :
+        print('this number not found')
+def repeat_op():
+    while True :
+        try :
+            QUESTION = input('If you want another operation please enter : [Yes] \nIf you want to stop please enter [No]\n')
+            line()
+            if QUESTION == 'Yes':
+                choice()
+                line()
+            elif QUESTION == 'No':
+                print ('We are happy to serve you')
+                line()
+                break
+            else :
+                print ('Check your input please')
+                line()
+        except:
+            continue
+def line():
+    print('------------------------------------------------------')
+def main():
+    choice()
+    line()
+    repeat_op()
+main()
